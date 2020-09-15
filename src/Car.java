d0abeab7caab8c5feb1b36998476a37370194288
flavio <flaviocr2012@gmail.com>
2020-09-15 12:59:23 -0300

    constructor added

--- src/Car.java
+++ src/Car.java
@@ -5,16 +5,26 @@ public class Car {
     int price;
     int seats;
 
-    // Creating methods
+    // constructors - special method - create objects - implicitly called.
 
-    public void carStart(){
-        System.out.println("Car Started");
+    //this -> reference to whoever is using it.
+    //class that using the "this" keyword.
+
+    public Car(double mileages, int prices, int seats) {
+        this.mileage = mileages; //set class's field mileage to be equal to mileage from method.
+        this.price = prices;
+        this.seats = seats;
 
     }
 
-    public void carShuttOff(){
-        System.out.println("Car Engine off");
+    // Creating methods
 
+    public void carStart() {
+        System.out.println("Car Started");
+    }
+
+    public void carShuttOff() {
+        System.out.println("Car Engine off");
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Car benz = new Car();
-        benz.mileage = 11.45;
-        benz.price = 35000;
-        benz.seats = 5;
+        Car benz = new Car (13.45, 40000, 3);
 
         System.out.println("The mileage being "+ benz.mileage);
         System.out.println("Benz is priced at " + benz.price + " dollars");
